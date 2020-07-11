7edcc183d5644ca01bff74b3572374ac52b81c9f
czara804 <cbzara@gmail.com>
2020-07-11 12:12:32 +1000

    file structure added to server and mongoose models for order and product
    Co-Authored-By: Jade <jade-tyrer@hotmail.co.uk>

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // Sets port if deploying to external provider 
 // or port assigned already
-const port = process.env.port || 3030;
+const port = process.env.port || 3000;
 
 // Equivalant of create server in http library
 const app = express();
@@ -13,10 +14,17 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+    }
+    
+const dbConn = process.env.MONGODB_URI || 'mongodb://localhost/bespoke_nails'  
+
+
 // Define a simple route for GET
 app.get("/",(req,res) => {
     res.send("Hi from your Express Server. From past you. You are awesome.")
 });
 
 // Listen
-app.listen(port, () => console.log(`Listening on port ${port}. Arrr.`));
+app.listen(port, () => console.log(`Listening on port ${port}.`));
