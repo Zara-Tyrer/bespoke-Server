5c0141a0196d257f3559e570b97ab8cfe91f166f
czara804 <cbzara@gmail.com>
2020-07-17 17:39:04 +1000

    error fixing authentication but it was nodemon
    Co-Authored-By: Jade <jade-tyrer@hotmail.co.uk>

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -20,9 +20,6 @@ const register = function (req, res) {
 
 const logout = function (req, res) {
     req.logout();
-    console.log('logged out user');
-    console.log('session object:', req.session);
-    console.log('req.user:', req.user);
     res.sendStatus(200);
 }
 
@@ -32,9 +29,6 @@ const authenticate = passport.authenticate('local');
 function loginUser(req, res) {
     // passport.authenticate returns a function that we will call with req, res, and a callback function to execute on success    
     authenticate(req, res, function () {
-        console.log('authenticated', req.user.username);
-        console.log('session object:', req.session);
-        console.log('req.user:', req.user);
         res.status(200);
         res.json(req.user);
     });
